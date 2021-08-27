02edd313587ae8b5db665cc786dc17107b628602
m293732 <70598864+m293732@users.noreply.github.com>
2021-08-27 12:07:47 -0500

    Add files via upload

---
+++ update-unbound-blacklist.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+BLACKLIST_FILE="/etc/unbound/unbound.conf.d/blacklist.conf"
+WHITELIST_FILE="/etc/unbound/whitelist"
+WHITELIST_URLS="
+	https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt
+	https://raw.githubusercontent.com/m293732/unbound-adblock-config/main/whitelist
+"
+BLACKLIST_URLS="
+	https://v.firebog.net/hosts/AdguardDNS.txt
+	https://v.firebog.net/hosts/Easyprivacy.txt
+	https://v.firebog.net/hosts/Prigent-Ads.txt
+	https://v.firebog.net/hosts/static/w3kbl.txt
+"
+function get_domains {
+	declare -a ARR=( ${!1} )
+	for i in "${ARR[@]}"; do
+		curl -sw '\n' "$i"
+	done | sed 's/#.*//;/^$/d' | sort -u
+}
+function convert_unbound {
+	while read domain; do
+		printf 'local-zone: "%s" always_nxdomain\n' "$domain"
+	done
+}
+umask 644
+printf '# Generated %s\n' "$(date)" > $WHITELIST_FILE
+get_domains "WHITELIST_URLS" >> $WHITELIST_FILE
+printf '# Generated %s\n' "$(date)" > $BLACKLIST_FILE
+get_domains "BLACKLIST_URLS" | grep -vf $WHITELIST_FILE | convert_unbound >> $BLACKLIST_FILE
+systemctl restart unbound.service
