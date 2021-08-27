b7c29916e9543fe2b6358977d2c73117734275de
m293732 <m293732@gmail.com>
2021-08-27 13:07:32 -0500

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+mkdir -p /etc/unbound/unbound.conf.d
 [ -f /etc/unbound/unbound.conf ] && mv /etc/unbound/unbound.conf /etc/unbound/unbound.conf.old
 install -Dt /etc/unbound -m644 ./unbound.conf
 install -Dt /lib/systemd/system -m644 ./systemd/update-unbound-blacklist.*
 install -Dt /usr/local/bin -m744 ./update-unbound-blacklist.sh
+systemctl daemon-reload
